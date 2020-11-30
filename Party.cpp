31d7446773837efc51e3ba4fc26de08bf4d7fa04
Aigals <70669489+Aigals@users.noreply.github.com>
2020-11-30 10:18:09 -0700

    Update Party.cpp

--- Party.cpp
+++ Party.cpp
@@ -56,7 +56,17 @@ void Party::setNumPartyMembers(int inc_numPartyMembers)
     numPartyMembers = inc_numPartyMembers;
 }
 
-void Party::removeMember()
+void Party::removeMember(int removeNumber)
 {
     numPartyMembers--;
+    //Loops replacing the removed member with the members after in the array
+    for(int i = removeNumber; i < partyNames.length(); i++)
+    {
+        if(i + 1 < partyNames.length())
+        {
+            partyNames[i] = partyNames[i + 1];
+        }
+    }
+    //Clears the last name to represent the loss
+    partyNames[4] = "";
 }
